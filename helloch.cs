da5504dac6cbb56bb2c93bcea4610d7dfa4339ca
Susanna Makaryan <69461145+Susi77@users.noreply.github.com>
2020-08-13 15:59:54 +0400

    Add files via upload

---
+++ helloch.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2Changeentrypoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+
+        static void SusiEntr()
+        {
+            Console.WriteLine("Hello World SusiEntry !!!");
+            Console.ReadKey();
+        }
+    }
+}
